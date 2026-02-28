62c8b4888f046126fa0bfc66acefe914b580442a
Camarederic <camarederic@gmail.com>
2026-02-28 20:19:03 +0300

    added code for the card JS

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-'use strict'
+"use strict";
 
+const container = document.querySelector(".container");
+const color1 = document.querySelector(".color-1");
+const color2 = document.querySelector(".color-2");
+
+color2.addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+color1.addEventListener("click", () => {
+  container.classList.remove("change");
+});
